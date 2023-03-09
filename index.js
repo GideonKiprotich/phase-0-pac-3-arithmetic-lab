9508c6bd141ba8f1a6ac3438f20362df791b2e10
Gideon Kiprotich <gnkiprotich@gmail.com>
2023-03-09 10:00:05 +0300

    Added arithmetic

---
+++ index.js
@@ -0,0 +1,49 @@
+function  add(){
+  return add();
+}
+function  subtract(){
+    return subtract();
+}
+
+function  multiply(){
+    return multiply();
+}
+
+function  divide(){
+   return divide(); 
+}
+
+function add(a,b){
+    return a+b;
+}
+add();
+function subtract(a,b){
+    return a-b;
+}
+subtract();
+function multiply(a,b){
+    return a*b;
+}
+multiply();
+function divide(a,b){
+    return a/b;
+}
+divide();
+
+function increment(a){
+    return ++a;
+}
+function decrement(b){
+    return --b;
+}
+function makeInt(n) {
+    parseInt('n');
+    return n;
+    
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
